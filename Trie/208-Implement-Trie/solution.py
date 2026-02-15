278e1c7bb53f9e1fe879ff1829095e8d2026b72f
Navneet Singh <38272714+navneet-singh2907@users.noreply.github.com>
2026-02-14 23:04:29 -0800

    Implement Trie with insert, search, and startsWith methods

---
+++ Trie/208-Implement-Trie/solution.py
@@ -0,0 +1,33 @@
+class TrieNode:
+    def __init__(self):
+        # Dictionary to store children nodes
+        self.children = {}
+        self.is_end = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word):
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+        node.is_end = True
+
+    def search(self, word):
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return node.is_end
+
+    def startsWith(self, prefix):
+        node = self.root
+        for char in prefix:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return True
