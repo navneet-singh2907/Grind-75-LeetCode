e12470cc61bb18de6603ba99af685939f2464238
Navneet Singh <Navnitgujjar@gmail.com>
2026-03-09 20:21:54 -0700

    Update solution.py

---
+++ Tree/297-Serialize-and-Deserialize-Binary-Tree/solution.py
@@ -0,0 +1,36 @@
+
+
+class Codec:
+
+    def serialize(self, root):
+        """Encodes a tree to a single string."""
+        res = []
+        
+        def dfs(node):
+            if not node:
+                res.append("N")
+                return
+            res.append(str(node.val))
+            dfs(node.left)
+            dfs(node.right)
+            
+        dfs(root)
+        return ",".join(res)
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree."""
+        vals = data.split(",")
+        self.i = 0 # Use an index to track position in the list
+        
+        def dfs():
+            if vals[self.i] == "N":
+                self.i += 1
+                return None
+            
+            node = TreeNode(int(vals[self.i]))
+            self.i += 1
+            node.left = dfs()
+            node.right = dfs()
+            return node
+            
+        return dfs()
